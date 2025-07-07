b197859054a2ae900dfeb2c40d0fa45d17e77d1c
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:22:01 +0500

    Update manageProfileRoutes.js

--- routes/supplierPortal/manageProfileRoutes.js
+++ routes/supplierPortal/manageProfileRoutes.js
@@ -1,1 +1,11 @@
-// manageProfile routes
+const express = require("express");
+const router = express.Router();
+const {
+  getProfile,
+  updateProfile
+} = require("../../controllers/supplierPortal/profileController");
+
+router.get("/", getProfile);
+router.put("/:id", updateProfile);
+
+module.exports = router;
