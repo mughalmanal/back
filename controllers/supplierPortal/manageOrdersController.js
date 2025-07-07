a1fdcb7cb9df521fc0c41a0a9daa8743cbb17a4c
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:36:37 +0500

    Update manageOrdersController.js

--- controllers/supplierPortal/manageOrdersController.js
+++ controllers/supplierPortal/manageOrdersController.js
@@ -1,1 +1,38 @@
-// manageOrders controller
+const ManageOrder = require("../../models/supplierPortal/ManageOrder");
+
+exports.createOrder = async (req, res) => {
+  try {
+    const order = new ManageOrder(req.body);
+    await order.save();
+    res.status(201).json(order);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await ManageOrder.find().sort({ createdAt: -1 });
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateOrder = async (req, res) => {
+  try {
+    const updated = await ManageOrder.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    await ManageOrder.findByIdAndDelete(req.params.id);
+    res.json({ message: "Order deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
