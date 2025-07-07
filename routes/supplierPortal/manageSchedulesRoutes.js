3704305571c9fd98edd4a589c24a4a9b59d6a681
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:18:12 +0500

    Update manageSchedulesRoutes.js

--- routes/supplierPortal/manageSchedulesRoutes.js
+++ routes/supplierPortal/manageSchedulesRoutes.js
@@ -1,1 +1,15 @@
-// manageSchedules routes
+const express = require("express");
+const router = express.Router();
+const {
+  addSchedule,
+  getSchedules,
+  updateSchedule,
+  deleteSchedule
+} = require("../../controllers/supplierPortal/manageSchedulesController");
+
+router.post("/", addSchedule);
+router.get("/", getSchedules);
+router.put("/:id", updateSchedule);
+router.delete("/:id", deleteSchedule);
+
+module.exports = router;
