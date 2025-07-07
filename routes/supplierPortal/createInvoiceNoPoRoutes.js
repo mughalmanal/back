abd2df8802dfaadde79bcc4f2f151b928396cd1b
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:23:58 +0500

    Update createInvoiceNoPoRoutes.js

--- routes/supplierPortal/createInvoiceNoPoRoutes.js
+++ routes/supplierPortal/createInvoiceNoPoRoutes.js
@@ -1,1 +1,15 @@
-// createInvoiceNoPo routes
+const express = require("express");
+const router = express.Router();
+const {
+  createInvoiceNoPo,
+  getInvoicesNoPo,
+  updateInvoiceNoPo,
+  deleteInvoiceNoPo
+} = require("../../controllers/supplierPortal/createInvoiceNoPoController");
+
+router.post("/", createInvoiceNoPo);
+router.get("/", getInvoicesNoPo);
+router.put("/:id", updateInvoiceNoPo);
+router.delete("/:id", deleteInvoiceNoPo);
+
+module.exports = router;
