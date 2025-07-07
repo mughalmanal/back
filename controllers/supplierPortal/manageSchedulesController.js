7e49c065ff0c23aeebfa0dad8745a4a1747f86ce
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:36:50 +0500

    Update manageSchedulesController.js

--- controllers/supplierPortal/manageSchedulesController.js
+++ controllers/supplierPortal/manageSchedulesController.js
@@ -1,1 +1,38 @@
-// manageSchedules controller
+const ManageSchedule = require("../../models/supplierPortal/ManageSchedule");
+
+exports.createSchedule = async (req, res) => {
+  try {
+    const schedule = new ManageSchedule(req.body);
+    await schedule.save();
+    res.status(201).json(schedule);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getSchedules = async (req, res) => {
+  try {
+    const schedules = await ManageSchedule.find().sort({ createdAt: -1 });
+    res.json(schedules);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateSchedule = async (req, res) => {
+  try {
+    const updated = await ManageSchedule.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteSchedule = async (req, res) => {
+  try {
+    await ManageSchedule.findByIdAndDelete(req.params.id);
+    res.json({ message: "Schedule deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
