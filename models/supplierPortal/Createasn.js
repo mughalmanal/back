e2c9fe332afcad8d9eff7d364fd57fe842cef835
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:29:33 +0500

    Update Createasn.js

--- models/supplierPortal/Createasn.js
+++ models/supplierPortal/Createasn.js
@@ -1,1 +1,15 @@
-// createASN model
+const mongoose = require("mongoose");
+
+const asnSchema = new mongoose.Schema({
+  asnNumber: { type: String, required: true },
+  shipmentNumber: String,
+  contents: [
+    {
+      item: String,
+      quantity: Number
+    }
+  ],
+  dateCreated: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+module.exports = mongoose.model("ASN", asnSchema);
