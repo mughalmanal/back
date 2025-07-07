8fc9c5edf3264a3f47d421473a72d7b67978db05
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:06:49 +0500

    Update Vendor.js

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,1 +1,12 @@
-// vendor model
+const mongoose = require("mongoose");
+
+const vendorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: String,
+  email: String,
+  address: String,
+  company: String,
+  gstNumber: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("Vendor", vendorSchema);
