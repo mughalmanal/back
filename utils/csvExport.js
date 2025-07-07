df0a0dd16cc2da05b2815773856b245e638cee09
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:42:36 +0500

    Update csvExport.js

--- utils/csvExport.js
+++ utils/csvExport.js
@@ -1,1 +1,14 @@
-// Utility for exporting CSV
+const { Parser } = require("json2csv");
+
+function exportToCSV(data, fields = []) {
+  try {
+    const opts = { fields };
+    const parser = new Parser(opts);
+    const csv = parser.parse(data);
+    return csv;
+  } catch (err) {
+    throw new Error("Failed to export CSV: " + err.message);
+  }
+}
+
+module.exports = exportToCSV;
