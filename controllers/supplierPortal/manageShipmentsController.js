4e9d631d545fc1503b0351bcda34db5c2915c0f5
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:38:22 +0500

    Update manageShipmentsController.js

--- controllers/supplierPortal/manageShipmentsController.js
+++ controllers/supplierPortal/manageShipmentsController.js
@@ -1,1 +1,38 @@
-// manageShipments controller
+const Shipment = require("../../models/supplierPortal/Shipment");
+
+exports.createShipment = async (req, res) => {
+  try {
+    const shipment = new Shipment(req.body);
+    await shipment.save();
+    res.status(201).json(shipment);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getShipments = async (req, res) => {
+  try {
+    const shipments = await Shipment.find().sort({ createdAt: -1 });
+    res.json(shipments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateShipment = async (req, res) => {
+  try {
+    const updated = await Shipment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteShipment = async (req, res) => {
+  try {
+    await Shipment.findByIdAndDelete(req.params.id);
+    res.json({ message: "Shipment deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
