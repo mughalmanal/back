5d9b21742c9b62813f61ba2416416169ebcbd848
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:20:08 +0500

    Update returnsRoutes.js

--- routes/supplierPortal/returnsRoutes.js
+++ routes/supplierPortal/returnsRoutes.js
@@ -1,1 +1,15 @@
-// returns routes
+const express = require("express");
+const router = express.Router();
+const {
+  createReturn,
+  getReturns,
+  updateReturn,
+  deleteReturn
+} = require("../../controllers/supplierPortal/returnsController");
+
+router.post("/", createReturn);
+router.get("/", getReturns);
+router.put("/:id", updateReturn);
+router.delete("/:id", deleteReturn);
+
+module.exports = router;
