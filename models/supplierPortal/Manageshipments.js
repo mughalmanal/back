20429b8e4229697be1b6d4260e18aa32cdc1767f
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:29:08 +0500

    Update Manageshipments.js

--- models/supplierPortal/Manageshipments.js
+++ models/supplierPortal/Manageshipments.js
@@ -1,1 +1,18 @@
-// manageShipments model
+const mongoose = require("mongoose");
+
+const shipmentSchema = new mongoose.Schema({
+  shipmentNumber: { type: String, required: true },
+  carrier: String,
+  trackingId: String,
+  status: { type: String, default: "In Transit" },
+  items: [
+    {
+      name: String,
+      quantity: Number
+    }
+  ],
+  shipDate: Date,
+  deliveryDate: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("Shipment", shipmentSchema);
