5700cf1f34b624e5970b18d9bb36ad007beaabdc
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:23:47 +0500

    Update createInvoiceRoutes.js

--- routes/supplierPortal/createInvoiceRoutes.js
+++ routes/supplierPortal/createInvoiceRoutes.js
@@ -1,1 +1,15 @@
-// createInvoice routes
+const express = require("express");
+const router = express.Router();
+const {
+  createInvoice,
+  getInvoices,
+  updateInvoice,
+  deleteInvoice
+} = require("../../controllers/supplierPortal/createInvoiceController");
+
+router.post("/", createInvoice);
+router.get("/", getInvoices);
+router.put("/:id", updateInvoice);
+router.delete("/:id", deleteInvoice);
+
+module.exports = router;
