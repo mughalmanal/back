6b4a74bc059867ef4ede16ce73f43fffd7a1e9ab
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:19:43 +0500

    Update uploadASNRoutes.js

--- routes/supplierPortal/uploadASNRoutes.js
+++ routes/supplierPortal/uploadASNRoutes.js
@@ -1,1 +1,13 @@
-// uploadASN routes
+const express = require("express");
+const router = express.Router();
+const {
+  uploadASN,
+  getUploadedASNs,
+  deleteUploadedASN
+} = require("../../controllers/supplierPortal/uploadASNController");
+
+router.post("/", uploadASN);
+router.get("/", getUploadedASNs);
+router.delete("/:id", deleteUploadedASN);
+
+module.exports = router;
