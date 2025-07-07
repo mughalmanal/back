e9acf651977fa68740877b8a2373e8fc524807b8
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:30:00 +0500

    Update Uploadasn.js

--- models/supplierPortal/Uploadasn.js
+++ models/supplierPortal/Uploadasn.js
@@ -1,1 +1,9 @@
-// uploadASN model
+const mongoose = require("mongoose");
+
+const uploadedASNschema = new mongoose.Schema({
+  fileName: String,
+  fileUrl: String,
+  uploadedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("UploadedASN", uploadedASNschema);
