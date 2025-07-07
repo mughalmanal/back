6041a85bdb25088ca07b644f75a7b01bfd79b937
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:15:51 +0500

    Update stockOutRoutes.js

--- routes/stockOutRoutes.js
+++ routes/stockOutRoutes.js
@@ -1,1 +1,15 @@
-// stockOut routes
+const express = require("express");
+const router = express.Router();
+const {
+  addStockOut,
+  getStockOut,
+  updateStockOut,
+  deleteStockOut,
+} = require("../controllers/stockOutController");
+
+router.post("/", addStockOut);
+router.get("/", getStockOut);
+router.put("/:id", updateStockOut);
+router.delete("/:id", deleteStockOut);
+
+module.exports = router;
