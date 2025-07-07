aa66baa870dbf7ca0126d452fdbfc27de26725b9
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:16:20 +0500

    Update purchaseOrderRoutes.js

--- routes/purchaseOrderRoutes.js
+++ routes/purchaseOrderRoutes.js
@@ -1,1 +1,15 @@
-// purchaseOrder routes
+const express = require("express");
+const router = express.Router();
+const {
+  addPurchaseOrder,
+  getPurchaseOrders,
+  updatePurchaseOrder,
+  deletePurchaseOrder,
+} = require("../controllers/purchaseOrderController");
+
+router.post("/", addPurchaseOrder);
+router.get("/", getPurchaseOrders);
+router.put("/:id", updatePurchaseOrder);
+router.delete("/:id", deletePurchaseOrder);
+
+module.exports = router;
