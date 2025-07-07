57138f50b863007e1715c255a1d1b1c23f22829d
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:16:41 +0500

    Update paymentEntryRoutes.js

--- routes/paymentEntryRoutes.js
+++ routes/paymentEntryRoutes.js
@@ -1,1 +1,15 @@
-// paymentEntry routes
+const express = require("express");
+const router = express.Router();
+const {
+  addPayment,
+  getPayments,
+  updatePayment,
+  deletePayment,
+} = require("../controllers/paymentEntryController");
+
+router.post("/", addPayment);
+router.get("/", getPayments);
+router.put("/:id", updatePayment);
+router.delete("/:id", deletePayment);
+
+module.exports = router;
