6e9a0ba817066696aad24b43eac187ab718fad8b
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:25:42 +0500

    Update viewInvoicesRoutes.js

--- routes/supplierPortal/viewInvoicesRoutes.js
+++ routes/supplierPortal/viewInvoicesRoutes.js
@@ -1,1 +1,9 @@
-// viewInvoices routes
+const express = require("express");
+const router = express.Router();
+const {
+  getAllInvoices
+} = require("../../controllers/supplierPortal/viewInvoicesController");
+
+router.get("/", getAllInvoices); // You can add filters or pagination here too
+
+module.exports = router;
