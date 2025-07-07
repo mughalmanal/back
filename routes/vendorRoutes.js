413ad92f1d34436bf4a0bfc6e847553fb67d075c
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:07:24 +0500

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,1 +1,16 @@
-// vendor routes
+const express = require("express");
+const router = express.Router();
+const {
+  addVendor,
+  getVendors,
+  updateVendor,
+  deleteVendor,
+} = require("../controllers/vendorController");
+
+// CRUD Routes
+router.post("/", addVendor);
+router.get("/", getVendors);
+router.put("/:id", updateVendor);
+router.delete("/:id", deleteVendor);
+
+module.exports = router;
