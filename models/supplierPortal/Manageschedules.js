2c2a27763af1275a66650c560296e5962fb89c8b
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:28:29 +0500

    Update Manageschedules.js

--- models/supplierPortal/Manageschedules.js
+++ models/supplierPortal/Manageschedules.js
@@ -1,1 +1,16 @@
-// manageSchedules model
+const mongoose = require("mongoose");
+
+const manageScheduleSchema = new mongoose.Schema({
+  scheduleId: { type: String, required: true },
+  orderNumber: String,
+  deliveryWindow: String,
+  items: [
+    {
+      name: String,
+      quantity: Number
+    }
+  ],
+  status: { type: String, default: "Scheduled" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("ManageSchedule", manageScheduleSchema);
