c703e446c7124529ad126a72ab1c570d50c476fe
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:38:51 +0500

    Update createASBNController.js

--- controllers/supplierPortal/createASBNController.js
+++ controllers/supplierPortal/createASBNController.js
@@ -1,1 +1,20 @@
-// createASBN controller
+const ASBN = require("../../models/supplierPortal/ASBN");
+
+exports.createASBN = async (req, res) => {
+  try {
+    const asbn = new ASBN(req.body);
+    await asbn.save();
+    res.status(201).json(asbn);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getASBNs = async (req, res) => {
+  try {
+    const asbns = await ASBN.find().sort({ createdAt: -1 });
+    res.json(asbns);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
