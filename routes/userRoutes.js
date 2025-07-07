54354184079b1e37c76b4db049ff3283fd0a5556
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:17:23 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,7 @@
-// user routes
+const express = require("express");
+const router = express.Router();
+const { loginUser } = require("../controllers/userController");
+
+router.post("/login", loginUser);
+
+module.exports = router;
