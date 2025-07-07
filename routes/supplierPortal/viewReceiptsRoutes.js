425248021c5c8eeb1b16dade0e4053f5d67f4887
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:19:55 +0500

    Update viewReceiptsRoutes.js

--- routes/supplierPortal/viewReceiptsRoutes.js
+++ routes/supplierPortal/viewReceiptsRoutes.js
@@ -1,1 +1,9 @@
-// viewReceipts routes
+const express = require("express");
+const router = express.Router();
+const {
+  getReceipts
+} = require("../../controllers/supplierPortal/viewReceiptsController");
+
+router.get("/", getReceipts);
+
+module.exports = router;
