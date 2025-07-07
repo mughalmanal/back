47c65aead922cc187face630139f14d7c49074fe
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:27:27 +0500

    Update Paymententry.js

--- models/Paymententry.js
+++ models/Paymententry.js
@@ -1,1 +1,11 @@
-// paymentEntry model
+const mongoose = require("mongoose");
+
+const paymentEntrySchema = new mongoose.Schema({
+  invoiceId: String,
+  amountPaid: Number,
+  paymentMethod: String,
+  paymentDate: { type: Date, default: Date.now },
+  remarks: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("PaymentEntry", paymentEntrySchema);
