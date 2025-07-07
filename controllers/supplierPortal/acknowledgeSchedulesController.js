c51f8996189d5de328b60e961939c317bf952b9c
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:37:02 +0500

    Update acknowledgeSchedulesController.js

--- controllers/supplierPortal/acknowledgeSchedulesController.js
+++ controllers/supplierPortal/acknowledgeSchedulesController.js
@@ -1,1 +1,38 @@
-// acknowledgeSchedules controller
+const AcknowledgeSchedule = require("../../models/supplierPortal/AcknowledgeSchedule");
+
+exports.acknowledgeSchedule = async (req, res) => {
+  try {
+    const ack = new AcknowledgeSchedule(req.body);
+    await ack.save();
+    res.status(201).json(ack);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getAcknowledgedSchedules = async (req, res) => {
+  try {
+    const acks = await AcknowledgeSchedule.find().sort({ createdAt: -1 });
+    res.json(acks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateAcknowledgedSchedule = async (req, res) => {
+  try {
+    const updated = await AcknowledgeSchedule.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteAcknowledgedSchedule = async (req, res) => {
+  try {
+    await AcknowledgeSchedule.findByIdAndDelete(req.params.id);
+    res.json({ message: "Acknowledged schedule deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
