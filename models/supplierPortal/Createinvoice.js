ef34cd3a2970a521004ee3ad0ad567d2d8a54ac0
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:30:57 +0500

    Update Createinvoice.js

--- models/supplierPortal/Createinvoice.js
+++ models/supplierPortal/Createinvoice.js
@@ -1,1 +1,18 @@
-// createInvoice model
+const mongoose = require("mongoose");
+
+const supplierInvoiceSchema = new mongoose.Schema({
+  invoiceNumber: { type: String, required: true },
+  vendor: String,
+  items: [
+    {
+      name: String,
+      quantity: Number,
+      price: Number
+    }
+  ],
+  totalAmount: Number,
+  status: { type: String, default: "Pending" },
+  invoiceDate: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("SupplierInvoice", supplierInvoiceSchema);
