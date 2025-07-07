b0e7f2ce9f8bd5842dc28c39520dbab899f51930
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:40:53 +0500

    Update viewPaymentsController.js

--- controllers/supplierPortal/viewPaymentsController.js
+++ controllers/supplierPortal/viewPaymentsController.js
@@ -1,1 +1,20 @@
-// viewPayments controller
+const SupplierPayment = require("../../models/supplierPortal/SupplierPayment");
+
+exports.addPayment = async (req, res) => {
+  try {
+    const payment = new SupplierPayment(req.body);
+    await payment.save();
+    res.status(201).json(payment);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getPayments = async (req, res) => {
+  try {
+    const payments = await SupplierPayment.find().sort({ createdAt: -1 });
+    res.json(payments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
