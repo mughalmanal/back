00710725ec840892534d50d0bb7433c9c3bfb496
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:15:37 +0500

    Update stockInRoutes.js

--- routes/stockInRoutes.js
+++ routes/stockInRoutes.js
@@ -1,1 +1,15 @@
-// stockIn routes
+const express = require("express");
+const router = express.Router();
+const {
+  addStockIn,
+  getStockIn,
+  updateStockIn,
+  deleteStockIn,
+} = require("../controllers/stockInController");
+
+router.post("/", addStockIn);
+router.get("/", getStockIn);
+router.put("/:id", updateStockIn);
+router.delete("/:id", deleteStockIn);
+
+module.exports = router;
