2d32da9dd804f328844deba47a089935ab52a85f
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:18:24 +0500

    Update acknowledgeSchedulesRoutes.js

--- routes/supplierPortal/acknowledgeSchedulesRoutes.js
+++ routes/supplierPortal/acknowledgeSchedulesRoutes.js
@@ -1,1 +1,15 @@
-// acknowledgeSchedules routes
+const express = require("express");
+const router = express.Router();
+const {
+  acknowledgeSchedule,
+  getAcknowledged,
+  updateAcknowledged,
+  deleteAcknowledged
+} = require("../../controllers/supplierPortal/acknowledgeSchedulesController");
+
+router.post("/", acknowledgeSchedule);
+router.get("/", getAcknowledged);
+router.put("/:id", updateAcknowledged);
+router.delete("/:id", deleteAcknowledged);
+
+module.exports = router;
