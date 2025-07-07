7f4be04e7c08072d9241830b01d6377340523871
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:05:47 +0500

    Update Client.js

--- models/Client.js
+++ models/Client.js
@@ -1,1 +1,19 @@
-// client model
+const mongoose = require("mongoose");
+
+const clientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: String,
+  email: String,
+  address: String,
+  type: { type: String, default: "Retail" },
+  cnic: String,
+  products: [
+    {
+      name: String,
+      price: Number,
+      quantity: Number,
+    }
+  ],
+}, { timestamps: true });
+
+module.exports = mongoose.model("Client", clientSchema);
