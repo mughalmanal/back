932b7eee3b8af42a4aa2415433778de86d4b820b
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:19:17 +0500

    Update createASNRoutes.js

--- routes/supplierPortal/createASNRoutes.js
+++ routes/supplierPortal/createASNRoutes.js
@@ -1,1 +1,15 @@
-// createASN routes
+const express = require("express");
+const router = express.Router();
+const {
+  createASN,
+  getASNs,
+  updateASN,
+  deleteASN
+} = require("../../controllers/supplierPortal/createASNController");
+
+router.post("/", createASN);
+router.get("/", getASNs);
+router.put("/:id", updateASN);
+router.delete("/:id", deleteASN);
+
+module.exports = router;
