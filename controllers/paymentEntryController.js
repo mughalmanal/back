20d700a07ed9400435790339ffffedf8ae3154c3
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:35:47 +0500

    Update paymentEntryController.js

--- controllers/paymentEntryController.js
+++ controllers/paymentEntryController.js
@@ -1,1 +1,29 @@
-// paymentEntry controller
+const PaymentEntry = require("../models/PaymentEntry");
+
+exports.addPayment = async (req, res) => {
+  try {
+    const payment = new PaymentEntry(req.body);
+    await payment.save();
+    res.status(201).json(payment);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getPayments = async (req, res) => {
+  try {
+    const payments = await PaymentEntry.find().sort({ createdAt: -1 });
+    res.json(payments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.deletePayment = async (req, res) => {
+  try {
+    await PaymentEntry.findByIdAndDelete(req.params.id);
+    res.json({ message: "Payment deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
