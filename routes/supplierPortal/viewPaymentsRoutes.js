f2ce600b65881c62a08f0bb34d4b1463af717872
mughalmanal <mughlu16029@gmail.com>
2025-07-07 20:14:23 +0500

    Update viewPaymentsRoutes.js

--- routes/supplierPortal/viewPaymentsRoutes.js
+++ routes/supplierPortal/viewPaymentsRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createViewPayments, getViewPayments, updateViewPayments, deleteViewPayments
-} = require("../controllers/ViewPaymentsController");
+const controller = require("../../controllers/SupplierPortal/viewPaymentsController");
 
-router.post("/", createViewPayments);
-router.get("/", getViewPayments);
-router.put("/:id", updateViewPayments);
-router.delete("/:id", deleteViewPayments);
+router.post("/", controller.create);
+router.get("/", controller.getAll);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.remove);
 
 module.exports = router;
