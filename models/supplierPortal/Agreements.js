674b5c26b836d8a129bbc25b300f3f1819fda330
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:28:52 +0500

    Update Agreements.js

--- models/supplierPortal/Agreements.js
+++ models/supplierPortal/Agreements.js
@@ -1,1 +1,12 @@
-// agreements model
+const mongoose = require("mongoose");
+
+const agreementSchema = new mongoose.Schema({
+  title: String,
+  vendor: String,
+  startDate: Date,
+  endDate: Date,
+  status: { type: String, enum: ["Active", "Expired"], default: "Active" },
+  fileUrl: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Agreement", agreementSchema);
