d63f9925a4fbda23b6c2fa62ccc10180eefccfc2
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:26:42 +0500

    Update Stockout.js

--- models/Stockout.js
+++ models/Stockout.js
@@ -1,1 +1,11 @@
-// stockOut model
+const mongoose = require("mongoose");
+
+const stockOutSchema = new mongoose.Schema({
+  product: { type: String, required: true },
+  quantity: Number,
+  date: { type: Date, default: Date.now },
+  client: String,
+  referenceNo: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("StockOut", stockOutSchema);
