92f93da77ade9b1b0f213f779a7b32c2358fbb15
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:28:40 +0500

    Update Acknowledgeschedules.js

--- models/supplierPortal/Acknowledgeschedules.js
+++ models/supplierPortal/Acknowledgeschedules.js
@@ -1,1 +1,16 @@
-// acknowledgeSchedules model
+const mongoose = require("mongoose");
+
+const acknowledgeScheduleSchema = new mongoose.Schema({
+  scheduleId: { type: String, required: true },
+  acknowledgedBy: String,
+  items: [
+    {
+      name: String,
+      quantity: Number
+    }
+  ],
+  notes: String,
+  status: { type: String, default: "Acknowledged" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("AcknowledgeSchedule", acknowledgeScheduleSchema);
