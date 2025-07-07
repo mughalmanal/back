0cdbad899167329166170dfe1ff1aca8f32fa54f
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:31:49 +0500

    Update Manageprofile.js

--- models/supplierPortal/Manageprofile.js
+++ models/supplierPortal/Manageprofile.js
@@ -1,1 +1,13 @@
-// manageProfile model
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  companyName: String,
+  contactName: String,
+  email: String,
+  phone: String,
+  address: String,
+  gstNumber: String,
+  ntnNumber: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("SupplierProfile", profileSchema);
