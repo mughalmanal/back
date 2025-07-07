ce01bb37c3592b438c1524d10a58e640719519c5
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:31:36 +0500

    Update Reviewconsumption.js

--- models/supplierPortal/Reviewconsumption.js
+++ models/supplierPortal/Reviewconsumption.js
@@ -1,1 +1,10 @@
-// reviewConsumption model
+const mongoose = require("mongoose");
+
+const consumptionSchema = new mongoose.Schema({
+  item: String,
+  usedQuantity: Number,
+  date: Date,
+  recordedBy: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Consumption", consumptionSchema);
