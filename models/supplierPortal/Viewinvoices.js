8dded5310e1aea7d9afb5be07de39b8265fc335c
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:34:13 +0500

    Update Viewinvoices.js

--- models/supplierPortal/Viewinvoices.js
+++ models/supplierPortal/Viewinvoices.js
@@ -1,1 +1,11 @@
-// viewInvoices model
+// viewInvoicesController.js
+const SupplierInvoice = require("../../models/supplierPortal/SupplierInvoice");
+
+exports.getAllInvoices = async (req, res) => {
+  try {
+    const invoices = await SupplierInvoice.find().sort({ createdAt: -1 });
+    res.json(invoices);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
