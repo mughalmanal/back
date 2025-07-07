fabe9fdca8620f6fe42f84bb214568d045f0bd70
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:30:31 +0500

    Update Returns.js

--- models/supplierPortal/Returns.js
+++ models/supplierPortal/Returns.js
@@ -1,1 +1,12 @@
-// returns model
+const mongoose = require("mongoose");
+
+const returnSchema = new mongoose.Schema({
+  returnNumber: { type: String, required: true },
+  shipmentNumber: String,
+  returnDate: Date,
+  quantity: Number,
+  reason: String,
+  refundRequested: Boolean
+}, { timestamps: true });
+
+module.exports = mongoose.model("Return", returnSchema);
