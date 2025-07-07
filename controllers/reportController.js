fa72e4537ec0170e1124fb8f83d43b7db33ade3c
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:35:59 +0500

    Update reportController.js

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,1 +1,20 @@
-// report controller
+const Report = require("../models/Report");
+
+exports.generateReport = async (req, res) => {
+  try {
+    const report = new Report(req.body);
+    await report.save();
+    res.status(201).json(report);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getReports = async (req, res) => {
+  try {
+    const reports = await Report.find().sort({ createdAt: -1 });
+    res.json(reports);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
