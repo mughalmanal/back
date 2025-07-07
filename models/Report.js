f0aabc8a3a7938836c1ee3ec9ab2495e29607084
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:27:42 +0500

    Update Report.js

--- models/Report.js
+++ models/Report.js
@@ -1,1 +1,12 @@
-// report model
+// This can be virtual (no model needed), but here's a basic schema if storing reports
+
+const mongoose = require("mongoose");
+
+const reportSchema = new mongoose.Schema({
+  title: String,
+  generatedOn: { type: Date, default: Date.now },
+  type: String,
+  data: mongoose.Schema.Types.Mixed
+});
+
+module.exports = mongoose.model("Report", reportSchema);
