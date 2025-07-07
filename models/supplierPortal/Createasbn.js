6b340cc6811f6a721b52495597cfd756fd9519b0
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:29:47 +0500

    Update Createasbn.js

--- models/supplierPortal/Createasbn.js
+++ models/supplierPortal/Createasbn.js
@@ -1,1 +1,15 @@
-// createASBN model
+const mongoose = require("mongoose");
+
+const asbnSchema = new mongoose.Schema({
+  asbnNumber: { type: String, required: true },
+  shipmentReference: String,
+  contents: [
+    {
+      item: String,
+      quantity: Number
+    }
+  ],
+  dispatchedAt: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("ASBN", asbnSchema);
