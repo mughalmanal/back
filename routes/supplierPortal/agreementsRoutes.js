9777218a3e2a53bf8a2f2276a2b3e4fce4a462f0
mughalmanal <mughlu16029@gmail.com>
2025-07-07 20:18:35 +0500

    Create agreementsRoutes.js

---
+++ routes/supplierPortal/agreementsRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const agreementsController = require("../../controllers/SupplierPortal/manageAgreementsController");
+
+// Create
+router.post("/", agreementsController.create);
+
+// Read all
+router.get("/", agreementsController.getAll);
+
+// Update
+router.put("/:id", agreementsController.update);
+
+// Delete
+router.delete("/:id", agreementsController.remove);
+
+module.exports = router;
