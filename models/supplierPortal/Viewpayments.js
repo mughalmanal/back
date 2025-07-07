0e327c4e550b3dbb8d241034d6e10abb694b8ee1
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:31:25 +0500

    Update Viewpayments.js

--- models/supplierPortal/Viewpayments.js
+++ models/supplierPortal/Viewpayments.js
@@ -1,1 +1,12 @@
-// viewPayments model
+const mongoose = require("mongoose");
+
+const supplierPaymentSchema = new mongoose.Schema({
+  paymentId: { type: String, required: true },
+  invoiceNumber: String,
+  vendor: String,
+  amount: Number,
+  paymentMethod: String,
+  paymentDate: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("SupplierPayment", supplierPaymentSchema);
