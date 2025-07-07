952a8eb8e456decad1d4aeafde5a6fdbb994a89b
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:28:17 +0500

    Update Manageorders.js

--- models/supplierPortal/Manageorders.js
+++ models/supplierPortal/Manageorders.js
@@ -1,1 +1,17 @@
-// manageOrders model
+const mongoose = require("mongoose");
+
+const manageOrderSchema = new mongoose.Schema({
+  orderNumber: { type: String, required: true },
+  buyer: String,
+  items: [
+    {
+      name: String,
+      quantity: Number,
+      unitPrice: Number
+    }
+  ],
+  status: { type: String, default: "Pending" },
+  deliveryDate: Date,
+}, { timestamps: true });
+
+module.exports = mongoose.model("ManageOrder", manageOrderSchema);
