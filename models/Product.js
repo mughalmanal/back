a73faec27e79d7ba0dfa8b20248a6702ddca10e4
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:14:10 +0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,15 @@
-// product model
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: String,
+  price: Number,
+  quantity: Number,
+  description: String,
+  imageUrl: String,
+  brand: String,
+  size: String,
+  color: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
