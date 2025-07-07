337137fe78211e5677cc64266b65fc5e73666312
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:30:13 +0500

    Update Viewreceipts.js

--- models/supplierPortal/Viewreceipts.js
+++ models/supplierPortal/Viewreceipts.js
@@ -1,1 +1,15 @@
-// viewReceipts model
+const mongoose = require("mongoose");
+
+const receiptSchema = new mongoose.Schema({
+  receiptNumber: { type: String, required: true },
+  shipmentNumber: String,
+  items: [
+    {
+      name: String,
+      quantity: Number
+    }
+  ],
+  receivedDate: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("Receipt", receiptSchema);
