db26c67614bfe10621b199b07a2cda841efe3829
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:18:36 +0500

    Update ordersViewRoutes.js

--- routes/supplierPortal/ordersViewRoutes.js
+++ routes/supplierPortal/ordersViewRoutes.js
@@ -1,1 +1,9 @@
-// ordersView routes
+const express = require("express");
+const router = express.Router();
+const {
+  getAllOrderViews
+} = require("../../controllers/supplierPortal/ordersViewController");
+
+router.get("/", getAllOrderViews);
+
+module.exports = router;
