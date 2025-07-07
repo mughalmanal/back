3a8f9d52a72d10fce483645aa73f6300bd8d280f
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:16:06 +0500

    Update invoiceRoutes.js

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,1 +1,15 @@
-// invoice routes
+const express = require("express");
+const router = express.Router();
+const {
+  addInvoice,
+  getInvoices,
+  updateInvoice,
+  deleteInvoice,
+} = require("../controllers/invoiceController");
+
+router.post("/", addInvoice);
+router.get("/", getInvoices);
+router.put("/:id", updateInvoice);
+router.delete("/:id", deleteInvoice);
+
+module.exports = router;
