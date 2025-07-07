77ce72ac07d68747b1f4cd561e634b8e9de949df
mughalmanal <mughlu16029@gmail.com>
2025-07-07 20:11:21 +0500

    Update createASBNRoutes.js

--- routes/supplierPortal/createASBNRoutes.js
+++ routes/supplierPortal/createASBNRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createCreateASBN, getCreateASBN, updateCreateASBN, deleteCreateASBN
-} = require("../controllers/CreateASBNController");
+const controller = require("../../controllers/SupplierPortal/createASBNController");
 
-router.post("/", createCreateASBN);
-router.get("/", getCreateASBN);
-router.put("/:id", updateCreateASBN);
-router.delete("/:id", deleteCreateASBN);
+router.post("/", controller.create);
+router.get("/", controller.getAll);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.remove);
 
 module.exports = router;
