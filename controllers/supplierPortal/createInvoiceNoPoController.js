dc17c6d655475ad3a34c70292d780816ec27764a
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:40:24 +0500

    Update createInvoiceNoPoController.js

--- controllers/supplierPortal/createInvoiceNoPoController.js
+++ controllers/supplierPortal/createInvoiceNoPoController.js
@@ -1,1 +1,20 @@
-// createInvoiceNoPo controller
+const SupplierInvoiceNoPo = require("../../models/supplierPortal/SupplierInvoiceNoPo");
+
+exports.createNoPoInvoice = async (req, res) => {
+  try {
+    const invoice = new SupplierInvoiceNoPo(req.body);
+    await invoice.save();
+    res.status(201).json(invoice);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getNoPoInvoices = async (req, res) => {
+  try {
+    const invoices = await SupplierInvoiceNoPo.find().sort({ createdAt: -1 });
+    res.json(invoices);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
