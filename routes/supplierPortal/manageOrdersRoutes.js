79591078acba6487656ebbe79a524ac184cd4ec6
mughalmanal <mughlu16029@gmail.com>
2025-07-07 19:52:24 +0500

    Update manageOrdersRoutes.js

--- routes/supplierPortal/manageOrdersRoutes.js
+++ routes/supplierPortal/manageOrdersRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createManageOrders, getManageOrders, updateManageOrders, deleteManageOrders
-} = require("../controllers/manageOrdersController");
 
+const {
+  createManageOrders,
+  getManageOrders,
+  updateManageOrders,
+  deleteManageOrders,
+} = require("../../controllers/SupplierPortal/manageOrdersController"); // ✅ Correct path
+
+// Routes
 router.post("/", createManageOrders);
 router.get("/", getManageOrders);
 router.put("/:id", updateManageOrders);
