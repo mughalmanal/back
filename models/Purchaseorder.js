f0a5fe19a2f3da72b419a6d92f349070ea2be999
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:27:16 +0500

    Update Purchaseorder.js

--- models/Purchaseorder.js
+++ models/Purchaseorder.js
@@ -1,1 +1,18 @@
-// purchaseOrder model
+const mongoose = require("mongoose");
+
+const purchaseOrderSchema = new mongoose.Schema({
+  orderNumber: { type: String, required: true },
+  vendor: String,
+  items: [
+    {
+      name: String,
+      quantity: Number,
+      price: Number
+    }
+  ],
+  totalAmount: Number,
+  status: { type: String, default: "Open" },
+  date: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+module.exports = mongoose.model("PurchaseOrder", purchaseOrderSchema);
