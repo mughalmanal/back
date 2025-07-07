8a28ac864facbc5fe754a36045526b7908e3ca61
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:06:07 +0500

    Update clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,1 +1,42 @@
-// client controller
+const Client = require("../models/Client");
+
+// CREATE
+exports.addClient = async (req, res) => {
+  try {
+    const client = new Client(req.body);
+    await client.save();
+    res.status(201).json(client);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// READ ALL
+exports.getClients = async (req, res) => {
+  try {
+    const clients = await Client.find().sort({ createdAt: -1 });
+    res.json(clients);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// UPDATE
+exports.updateClient = async (req, res) => {
+  try {
+    const updated = await Client.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// DELETE
+exports.deleteClient = async (req, res) => {
+  try {
+    await Client.findByIdAndDelete(req.params.id);
+    res.json({ message: "Client deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
