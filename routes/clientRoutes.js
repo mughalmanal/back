abb968b65325ffb4fabf935ecab3872cdc449988
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:06:22 +0500

    Update clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,1 +1,16 @@
-// client routes
+const express = require("express");
+const router = express.Router();
+const {
+  addClient,
+  getClients,
+  updateClient,
+  deleteClient,
+} = require("../controllers/clientController");
+
+// CRUD Routes
+router.post("/", addClient);
+router.get("/", getClients);
+router.put("/:id", updateClient);
+router.delete("/:id", deleteClient);
+
+module.exports = router;
