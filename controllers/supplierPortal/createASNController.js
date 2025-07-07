6c554a75944649a3a4bf3228394eaa293cdf4c69
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:38:39 +0500

    Update createASNController.js

--- controllers/supplierPortal/createASNController.js
+++ controllers/supplierPortal/createASNController.js
@@ -1,1 +1,20 @@
-// createASN controller
+const ASN = require("../../models/supplierPortal/ASN");
+
+exports.createASN = async (req, res) => {
+  try {
+    const asn = new ASN(req.body);
+    await asn.save();
+    res.status(201).json(asn);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getASNs = async (req, res) => {
+  try {
+    const asns = await ASN.find().sort({ createdAt: -1 });
+    res.json(asns);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
