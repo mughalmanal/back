6077aef0c458867e23a82b6fa7d08af3a9fff1c4
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:35:14 +0500

    Update invoiceController.js

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -1,1 +1,38 @@
-// invoice controller
+const Invoice = require("../models/Invoice");
+
+exports.addInvoice = async (req, res) => {
+  try {
+    const invoice = new Invoice(req.body);
+    await invoice.save();
+    res.status(201).json(invoice);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getInvoices = async (req, res) => {
+  try {
+    const invoices = await Invoice.find().sort({ createdAt: -1 });
+    res.json(invoices);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateInvoice = async (req, res) => {
+  try {
+    const updated = await Invoice.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteInvoice = async (req, res) => {
+  try {
+    await Invoice.findByIdAndDelete(req.params.id);
+    res.json({ message: "Invoice deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
