9ad019b1fb0af4a941a490689268aeb308b6a803
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:31:15 +0500

    Update Createinvoicenopo.js

--- models/supplierPortal/Createinvoicenopo.js
+++ models/supplierPortal/Createinvoicenopo.js
@@ -1,1 +1,12 @@
-// createInvoiceNoPo model
+const mongoose = require("mongoose");
+
+const supplierInvoiceNoPoSchema = new mongoose.Schema({
+  invoiceNumber: { type: String, required: true },
+  vendor: String,
+  description: String,
+  amount: Number,
+  status: { type: String, default: "Unpaid" },
+  invoiceDate: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("SupplierInvoiceNoPo", supplierInvoiceNoPoSchema);
