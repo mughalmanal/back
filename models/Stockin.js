b46097c080b2119672f2e8d8716364f42fbe6b45
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:26:32 +0500

    Update Stockin.js

--- models/Stockin.js
+++ models/Stockin.js
@@ -1,1 +1,11 @@
-// stockIn model
+const mongoose = require("mongoose");
+
+const stockInSchema = new mongoose.Schema({
+  product: { type: String, required: true },
+  quantity: Number,
+  date: { type: Date, default: Date.now },
+  vendor: String,
+  referenceNo: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("StockIn", stockInSchema);
