5c7584fc25ec94a6332d725b2ad0d231443673fe
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:24:38 +0500

    Update viewPaymentsRoutes.js

--- routes/supplierPortal/viewPaymentsRoutes.js
+++ routes/supplierPortal/viewPaymentsRoutes.js
@@ -1,1 +1,13 @@
-// viewPayments routes
+const express = require("express");
+const router = express.Router();
+const {
+  createSupplierPayment,
+  getSupplierPayments,
+  deleteSupplierPayment
+} = require("../../controllers/supplierPortal/paymentsController");
+
+router.post("/", createSupplierPayment);
+router.get("/", getSupplierPayments);
+router.delete("/:id", deleteSupplierPayment);
+
+module.exports = router;
