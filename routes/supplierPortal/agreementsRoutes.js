86d71fa2ee1285e01684fcdc60208e7fac2152aa
mughalmanal <mughlu16029@gmail.com>
2025-07-07 16:18:47 +0500

    Update agreementsRoutes.js

--- routes/supplierPortal/agreementsRoutes.js
+++ routes/supplierPortal/agreementsRoutes.js
@@ -1,1 +1,15 @@
-// agreements routes
+const express = require("express");
+const router = express.Router();
+const {
+  uploadAgreement,
+  getAgreements,
+  deleteAgreement,
+  updateAgreement
+} = require("../../controllers/supplierPortal/agreementsController");
+
+router.post("/", uploadAgreement);
+router.get("/", getAgreements);
+router.put("/:id", updateAgreement);
+router.delete("/:id", deleteAgreement);
+
+module.exports = router;
