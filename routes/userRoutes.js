689a745ca354b98760bed9f473034d248d0c0a30
mughalmanal <mughlu16029@gmail.com>
2025-07-08 14:14:15 +0500

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginUser } = require("../controllers/userController");
-
-router.post("/login", loginUser);
-
-export default router;
