e4be48b23263734fe5671bbdb153e7d8ba8ec318
mughalmanal <mughlu16029@gmail.com>
2025-07-08 13:39:02 +0500

    Update Client.js

--- models/Client.js
+++ models/Client.js
@@ -2,18 +2,13 @@ const mongoose = require("mongoose");
 
 const clientSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  phone: String,
-  email: String,
-  address: String,
-  type: { type: String, default: "Retail" },
-  cnic: String,
-  products: [
-    {
-      name: String,
-      price: Number,
-      quantity: Number,
-    }
-  ],
+  phone: { type: String },
+  email: { type: String },
+  address: { type: String },
+  type: { type: String, enum: ["Retail", "Wholesale"], default: "Retail" },
+  cnic: { type: String },
+  productBought: { type: String },
+  price: { type: Number },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Client", clientSchema);
